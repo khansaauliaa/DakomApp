1696a2de45c1b0773a71180146b5b03ef6572a75
Muhammad Rizandy Ar-ridha <44150729+MuhZanArd@users.noreply.github.com>
2022-02-24 14:31:02 +0700

    Initial commit

---
+++ app/src/main/java/com/muhzanard/dakomapp/LandingScreen.kt
@@ -0,0 +1,11 @@
+package com.muhzanard.dakomapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LandingScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing_screen)
+    }
+}
